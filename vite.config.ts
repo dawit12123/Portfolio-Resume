36ff4e320d1e112ff6a5088fbdb35001b69c9894
dawit12123 <84643623+dawit12123@users.noreply.github.com>
2026-08-22 01:43:59 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: ["portfolio-resume-7clm.onrender.com","dawit-girma.onrender.com"]
+  }
 })
